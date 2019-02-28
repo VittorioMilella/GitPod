fc0435b03031483d62aad28a7eeac3edb1dbab7f
EC2 Default User <ec2-user@ip-172-31-76-215.ec2.internal>
2019-02-28 14:36:06 +0000

    primo commit

---
+++ src/app/mock-cars.ts
@@ -0,0 +1,14 @@
+import { Car } from './car';
+
+export const CARS: Car[] = [
+  { id: 11, name: 'Putno' },
+  { id: 12, name: 'Bravo' },
+  { id: 13, name: 'Tipo' },
+  { id: 14, name: '124_spider' },
+  { id: 15, name: '500' },
+  { id: 16, name: 'Panda' },
+  { id: 17, name: 'Qubo' },
+  { id: 18, name: 'Doblo' },
+  { id: 19, name: 'Talento' },
+  { id: 20, name: 'Freemont' }
+];
