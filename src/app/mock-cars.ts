9e46055d8ad00b25faca75106b5e566e667c6b03
antlui64galvanimi <milella.vittorio@iisgalvanimi.edu.it>
2023-02-10 12:36:53 +0000

    commit_1

--- src/app/mock-cars.ts
+++ src/app/mock-cars.ts
@@ -1,9 +1,9 @@
 import { Car } from './car';
 
 export const CARS: Car[] = [
-  { id: 11, name: 'Putno', costo:10, consumo:25 },
-  { id: 12, name: 'Bravo', costo:20, consumo:20 }, 
-  { id: 13, name: 'Tipo', costo:15, consumo:10 }, 
-  { id: 14, name: '124_spider' , costo:100, consumo:90 },
-  { id: 15, name: '500', costo:1000, consumo:33 }
+  { id: 11, name: 'Lamborghini', costo:10, consumo:25 },
+  { id: 12, name: 'Punto', costo:20, consumo:20 }, 
+  { id: 13, name: 'Golf', costo:15, consumo:10 }, 
+  { id: 14, name: 'BMW' , costo:100, consumo:90 },
+  { id: 15, name: 'Subaru Baracca', costo:1000, consumo:33 }
 ];
